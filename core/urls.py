b27520b6f310b2418fb439b549b888ffefada074
Kevin Palma <kapalma@gmail.com>
2023-10-07 10:33:41 -0600

    :bug: Fix
    fix

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from .views import HomeView
 
 urlpatterns = [
-  path('/', HomeView.as_view(), name='home'),
-  path('login/', HomeView.as_view(), name='home'),
-  path('companies/', HomeView.as_view(), name='home'),
-  path('company/<int:pk>/', HomeView.as_view(), name='home'),
-  path('companies-create/', HomeView.as_view(), name='home'),
-  path('add-employee/', HomeView.as_view(), name='home'),
-  path('employees/', HomeView.as_view(), name='home'),
-  path('employee/<int:pk>/', HomeView.as_view(), name='home'),
-  path('store/', HomeView.as_view(), name='home'),
-  path('validate-buy/<int:pk>/', HomeView.as_view(), name='home'),
+  path('', HomeView.as_view(), name='home'),
+  path('login', HomeView.as_view(), name='home'),
+  path('companies', HomeView.as_view(), name='home'),
+  path('company/<int:pk>', HomeView.as_view(), name='home'),
+  path('companies-create', HomeView.as_view(), name='home'),
+  path('add-employee', HomeView.as_view(), name='home'),
+  path('employees', HomeView.as_view(), name='home'),
+  path('employee/<int:pk>', HomeView.as_view(), name='home'),
+  path('store', HomeView.as_view(), name='home'),
+  path('validate-buy/<int:pk>', HomeView.as_view(), name='home'),
 ]
